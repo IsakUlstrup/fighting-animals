561baaf45cdc998a8095e21edcf7e9cbe27fb5ea
Isak Ulstrup <isakulstrup@gmail.com>
2022-08-30 10:32:44 +0200

    added skill cooldown

--- src/Engine/Skill.elm
+++ src/Engine/Skill.elm
@@ -4,9 +4,10 @@ module Engine.Skill exposing (Skill, new)
 type alias Skill =
     { name : String
     , description : String
+    , cooldown : ( Int, Int )
     }
 
 
-new : String -> String -> Skill
-new name description =
-    Skill name description
+new : String -> String -> Int -> Skill
+new name description cooldown =
+    Skill name description ( 0, cooldown )
--- tests/Skill.elm
+++ tests/Skill.elm
@@ -2,8 +2,8 @@ module Skill exposing (suite)
 
 import Engine.Skill
 import Expect
-import Fuzz exposing (string)
-import Test exposing (Test, describe, fuzz2)
+import Fuzz exposing (int, string)
+import Test exposing (Test, describe, fuzz, fuzz2)
 
 
 suite : Test
@@ -12,10 +12,19 @@ suite =
         [ describe "New skill"
             [ fuzz2 string string "new skill with fuzz name and description" <|
                 \randomName randomDescription ->
-                    Engine.Skill.new randomName randomDescription
+                    Engine.Skill.new randomName randomDescription 1000
                         |> Expect.equal
                             { name = randomName
                             , description = randomDescription
+                            , cooldown = ( 0, 1000 )
+                            }
+            , fuzz int "new skill with fuzz cooldown" <|
+                \randomCooldown ->
+                    Engine.Skill.new "Name" "Description" randomCooldown
+                        |> Expect.equal
+                            { name = "Name"
+                            , description = "Description"
+                            , cooldown = ( 0, randomCooldown )
                             }
             ]
         ]
