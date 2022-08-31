6cd82d6248f328d42b46197a9ccc9d5c0c4b3d4f
Isak Ulstrup <isakulstrup@gmail.com>
2022-08-31 09:55:12 +0200

    updated skill descriptions

--- src/Content/Skills.elm
+++ src/Content/Skills.elm
@@ -5,9 +5,9 @@ import Engine.Skill as Skill exposing (Skill)
 
 basicSkill : Skill
 basicSkill =
-    Skill.new "Basic Skill" "Example skill" 1000
+    Skill.new "Basic Skill" "Just a basic skill, nothing special" 1000
 
 
 slowSkill : Skill
 slowSkill =
-    Skill.new "Slow Skill" "Slow skill, hopefully it hits hard" 6000
+    Skill.new "Slow Skill" "This skill is super slow, hopefully it hits hard" 6000
