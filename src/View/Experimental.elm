48c7b97bc1a2420b1cc741347c40639e67964145
Isak <isakulstrup@gmail.com>
2022-09-02 08:16:31 +0200

    extracted svg view code to its own module

--- src/View/Experimental.elm
+++ src/View/Experimental.elm
@@ -6,39 +6,7 @@ import Element.Border as Border
 import Element.Events
 import Element.Font as Font
 import Engine.Skill as Skill exposing (Skill)
-import Svg exposing (svg)
-import Svg.Attributes
-
-
-viewSpinner : Int -> Char -> Element msg
-viewSpinner percentage icon =
-    el [ Element.width <| px 40 ] <|
-        Element.html <|
-            svg
-                [ Svg.Attributes.viewBox "-1 -1 34 34" ]
-                [ Svg.circle
-                    [ Svg.Attributes.stroke "magenta"
-                    , Svg.Attributes.fill "none"
-                    , Svg.Attributes.transform "rotate(90, 16, 16)"
-                    , Svg.Attributes.strokeWidth "2"
-                    , Svg.Attributes.strokeLinecap "round"
-                    , Svg.Attributes.cx "16"
-                    , Svg.Attributes.cy "16"
-                    , Svg.Attributes.r "15.9155"
-                    , Svg.Attributes.shapeRendering "geometricPrecision"
-                    , Svg.Attributes.strokeDasharray ((clamp 0 100 percentage |> String.fromInt) ++ ", 100")
-                    ]
-                    []
-                , Svg.text_
-                    [ Svg.Attributes.x "16"
-                    , Svg.Attributes.y "16"
-                    , Svg.Attributes.textAnchor "middle"
-                    , Svg.Attributes.dominantBaseline "middle"
-                    , Svg.Attributes.fill "#262626"
-                    , Svg.Attributes.fontFamily "sans-serif"
-                    ]
-                    [ Svg.text <| String.fromChar icon ]
-                ]
+import View.Svg
 
 
 viewSkillButton : msg -> Skill -> Element msg
@@ -93,7 +61,9 @@ viewSkillButton clickMsg skill =
                , Element.width fill
                ]
         )
-        [ viewSpinner (Skill.cooldownPercentage skill) '★'
+        [ el [ Element.width <| px 40 ] <|
+            Element.html <|
+                View.Svg.viewSpinner (Skill.cooldownPercentage skill) '★'
         , Element.column [ Element.spacing 3 ]
             [ el [ Font.size 16 ] (text skill.name)
             , el
---
+++ src/View/Svg.elm
@@ -0,0 +1,33 @@
+module View.Svg exposing (viewSpinner)
+
+import Svg exposing (Svg, svg)
+import Svg.Attributes
+
+
+viewSpinner : Int -> Char -> Svg msg
+viewSpinner percentage icon =
+    svg
+        [ Svg.Attributes.viewBox "-1 -1 34 34" ]
+        [ Svg.circle
+            [ Svg.Attributes.stroke "magenta"
+            , Svg.Attributes.fill "none"
+            , Svg.Attributes.transform "rotate(90, 16, 16)"
+            , Svg.Attributes.strokeWidth "2"
+            , Svg.Attributes.strokeLinecap "round"
+            , Svg.Attributes.cx "16"
+            , Svg.Attributes.cy "16"
+            , Svg.Attributes.r "15.9155"
+            , Svg.Attributes.shapeRendering "geometricPrecision"
+            , Svg.Attributes.strokeDasharray ((clamp 0 100 percentage |> String.fromInt) ++ ", 100")
+            ]
+            []
+        , Svg.text_
+            [ Svg.Attributes.x "16"
+            , Svg.Attributes.y "16"
+            , Svg.Attributes.textAnchor "middle"
+            , Svg.Attributes.dominantBaseline "middle"
+            , Svg.Attributes.fill "#262626"
+            , Svg.Attributes.fontFamily "sans-serif"
+            ]
+            [ Svg.text <| String.fromChar icon ]
+        ]
