f8c14ee5ffff8ffb6a0261cb7598993f2c6da1ae
Isak <isakulstrup@gmail.com>
2022-09-02 11:26:15 +0200

    extracted helpers for skill button classes

--- src/View/ElmHtml.elm
+++ src/View/ElmHtml.elm
@@ -1,36 +1,46 @@
 module View.ElmHtml exposing (view)
 
 import Engine.Skill as Skill exposing (Skill, SkillEffect)
-import Html exposing (Html, div, h5, main_, p, text)
+import Html exposing (Attribute, Html, div, h5, main_, p, text)
 import Html.Attributes exposing (class, id)
 import Html.Events
 import View.Svg
+
+
+skillEffectClass : Skill -> Attribute msg
+skillEffectClass skill =
+    class <|
+        case skill.state of
+            Skill.Cooling _ ->
+                "skill-cooling"
+
+            Skill.Active _ ->
+                "skill-active"
+
+            Skill.Ready ->
+                "skill-ready"
+
+
+skillStateClass : Skill -> Attribute msg
+skillStateClass skill =
+    class <|
+        case skill.effect of
+            Skill.Hit _ ->
+                "skill-hit"
+
+            Skill.Buff _ ->
+                "skill-buff"
+
+            Skill.Debuff _ ->
+                "skill-debuff"
 
 
 viewSmallSkill : Skill -> Html msg
 viewSmallSkill skill =
     div
         [ class "small-skill"
-        , class <|
-            case skill.state of
-                Skill.Cooling _ ->
-                    "skill-cooling"
-
-                Skill.Active _ ->
-                    "skill-active"
-
-                Skill.Ready ->
-                    "skill-ready"
-        , class <|
-            case skill.effect of
-                Skill.Hit _ ->
-                    "skill-hit"
-
-                Skill.Buff _ ->
-                    "skill-buff"
-
-                Skill.Debuff _ ->
-                    "skill-debuff"
+        , skillStateClass skill
+        , skillEffectClass skill
         ]
         []
 
@@ -53,26 +63,8 @@ viewSkillButton clickMsg skill =
     Html.button
         [ Html.Events.onClick clickMsg
         , class "skill-button"
-        , class <|
-            case skill.state of
-                Skill.Cooling _ ->
-                    "skill-cooling"
-
-                Skill.Active _ ->
-                    "skill-active"
-
-                Skill.Ready ->
-                    "skill-ready"
-        , class <|
-            case skill.effect of
-                Skill.Hit _ ->
-                    "skill-hit"
-
-                Skill.Buff _ ->
-                    "skill-buff"
-
-                Skill.Debuff _ ->
-                    "skill-debuff"
+        , skillStateClass skill
+        , skillEffectClass skill
         ]
         [ View.Svg.viewSpinner (Skill.cooldownPercentage skill) icon
         , div [ class "skill-meta" ]
