4ee815cbf4dce58eb57f6686b9e555a227bd59a0
Isak <isakulstrup@gmail.com>
2022-09-03 21:33:26 +0200

    made skill effect view prettier

--- src/Main.elm
+++ src/Main.elm
@@ -78,7 +78,7 @@ update msg model =
                 ( skills, effects ) =
                     model.skills |> useSkill index
             in
-            ( { model | skills = skills, skillEffects = effects ++ model.skillEffects |> List.take 10 }, Cmd.none )
+            ( { model | skills = skills, skillEffects = effects ++ model.skillEffects |> List.take 50 }, Cmd.none )
 
         ShowQrModal ->
             ( { model
